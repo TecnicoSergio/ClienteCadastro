576837d866b7abe4f92dd5310c65560e7bcdbd7e
Sergio Rodrigues dos Santos <sergiotinfo3@gmail.com>
2025-11-10 11:02:04 -0300

    CommitAlteraçãoCodigo

--- supabasecliente.js
+++ supabasecliente.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://iccpktxyvhfbisxqpdkp.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImljY3BrdHF5eWhmYml4c3FwZGtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3NjI4MDYsImV4cCI6MjA3ODMzODgwNn0.VPug1NH3rXFAeeLFWel4tHLgXj9jFz1eKHlATu7-Qwc';
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
